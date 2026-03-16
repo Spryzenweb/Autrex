1431377c322d890d09c8cc75189f1685ecabbd4d
Spryzen <123577747+Spryzenweb@users.noreply.github.com>
2026-03-16 21:25:02 +0300

    Delete src directory

--- src/renderer/src/store/atoms/auth.atoms.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai'
-
-// Simplified auth atoms - old authentication system removed
-// License management is now handled by license.atoms.ts
-export const authTokenAtom = atom<string | null>(null)
--- src/renderer/src/store/atoms/settings.atoms.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'jotai'
-
-// Settings atoms
-export const leagueClientEnabledAtom = atom<boolean>(true)
-export const championDetectionEnabledAtom = atom<boolean>(true)
-export const smartApplyEnabledAtom = atom<boolean>(true)
-export const autoApplyEnabledAtom = atom<boolean>(true)
-export const autoApplyTriggerTimeAtom = atom<number>(15)
-export const p2pAutoSyncEnabledAtom = atom<boolean>(true)
